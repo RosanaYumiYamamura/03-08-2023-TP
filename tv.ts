5f3c3d57958460456b3654055ac5261b3c26627b
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-08-10 00:28:29 -0300

    new

--- tv.ts
+++ tv.ts
@@ -2,39 +2,62 @@ class Televisor {
     isOn: boolean;
     volume: number;
     channel: number;
+    currentTime: string;
     constructor(on: boolean = false, vol: number = 0, chn: number = 0) {
       this.isOn = on;
-      this.volume = vol;
-      this.channel = chn;
+      this.volume = Math.min(100, Math.max(0, vol)); 
+      this.channel = Math.min(99, Math.max(1, chn)); 
+      this.currentTime = new Date().toLocaleTimeString();
     }
     switchOnOff(): void {
       this.isOn = !this.isOn;
       console.log(this.isOn ? "encendido" : "apagado");
     }
     volUp(): void {
-      console.log("vol up");
+        if (this.volume < 100) {
+            this.volume++;
+            console.log("vol up");
+          }
     }
     volDown(): void {
-      console.log("vol down");
+        if (this.volume > 0) {
+            this.volume--;
+            console.log("vol down");
+          }
     }
     channelUp(): void {
-      console.log("channel up");
+        if (this.channel < 99) {
+            this.channel++;
+            console.log("channel up");
+          }
     }
     channelDown(): void {
-      console.log("channel down");
+        if (this.channel > 1) {
+            this.channel--;
+            console.log("channel down");
+          }
     }
     pickChannel(channel: number): void {
-      if (!this.isOn) return;
-      this.channel = channel;
-      console.log("Cambiaste al canal", this.channel);
-    }
+        if (!this.isOn) return;
+        this.channel = Math.min(99, Math.max(1, channel)); 
+        console.log("Cambiaste al canal", this.channel);
+      }
     info(): void {
-      console.log("canal, volumen, hora actual");
+        console.log("Canal:", this.channel, "Volumen:", this.volume, "Hora actual:", this.currentTime);
     }
   }
   
   const tv01 = new Televisor();
   tv01.switchOnOff();
-  tv01.switchOnOff();
-  tv01.switchOnOff();
-  tv01.pickChannel(45);
+  
+  // Cambiar el volumen
+  tv01.volUp(); 
+  tv01.volUp(); 
+  tv01.volDown(); 
+  
+  // Cambiar el canal
+  tv01.channelUp(); 
+  tv01.channelDown(); 
+  
+  // Mostrar información actual
+  tv01.info(); 
